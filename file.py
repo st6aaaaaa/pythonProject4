b97f2b94ecc186556982da8384edea44d7730c8c
Kirill <kirillnechaev3120@gmail.com>
2022-12-19 20:46:51 +0300

    message - message

---
+++ file.py
@@ -0,0 +1,12 @@
+def func(*args):
+    print( all( a> 0 for a in args) )
+
+func(1,2,3,0)
+func(1,3)
+
+
+def pc_branch():
+    print('hello')
+
+def func_github():
+    print("from github")
---
+++ main.py
@@ -0,0 +1,16 @@
+'kkkk'
+
+def sr(s):
+    return len(s) == 2
+def func(a,b):
+  return a + b 
+
+def func1(a,b):
+  return a % b == 2 
+
+from datetime import date
+
+d = date('2019-11-23')
+print(d)
+
+print('kkkk')
