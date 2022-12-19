67f8a6287722dfcc0323ca82ff569f5966181c5b
st6aaaaaa <110686768+st6aaaaaa@users.noreply.github.com>
2022-12-19 21:09:08 +0300

    Update file2.py

--- file2.py
+++ file2.py
@@ -7,10 +7,7 @@ def func(*args,**kwargs):
             return res*fu(a,b)
         return ins
     return qwer
-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
-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
-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
-trewq123POI
+
 @func(1,2,3,4,a=12,b='qwer')
 def kek(a,b):
     return a+b
