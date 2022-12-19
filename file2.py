6c4251691a3b218f87e0b3109818701774d16fb9
Kirill <kirillnechaev3120@gmail.com>
2022-12-19 21:07:03 +0300

    file2 com

---
+++ file2.py
@@ -0,0 +1,22 @@
+def func(*args,**kwargs):
+    def qwer(fu):
+        def ins(a,b):
+            res = 0
+            for i in args:
+                res+=i
+            return res*fu(a,b)
+        return ins
+    return qwer
+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
+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
+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
+trewq123POI
+@func(1,2,3,4,a=12,b='qwer')
+def kek(a,b):
+    return a+b
+
+import itertools as f
+
+word = ['a','b','c']
+a = f.accumulate(word)
+print( 'a' in a )
